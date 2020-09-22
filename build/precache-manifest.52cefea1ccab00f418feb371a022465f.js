b2059ce70bf3739ba516aa8dcff82f3121307b08
amarbessedik <amar.bessedik@gmail.com>
2020-09-21 22:49:06 -0400

    web app fully functional

---
+++ build/precache-manifest.52cefea1ccab00f418feb371a022465f.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccb1bdd2792b93382a6ad59a8192aa71",
+    "url": "/index.html"
+  },
+  {
+    "revision": "34cdc7a45f1d256b451e",
+    "url": "/static/css/2.a4567992.chunk.css"
+  },
+  {
+    "revision": "1c2e825df2cd0eb159c0",
+    "url": "/static/css/main.502e64ba.chunk.css"
+  },
+  {
+    "revision": "34cdc7a45f1d256b451e",
+    "url": "/static/js/2.67395ed6.chunk.js"
+  },
+  {
+    "revision": "63400942756c9636209f91bbb32d3e8b",
+    "url": "/static/js/2.67395ed6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c2e825df2cd0eb159c0",
+    "url": "/static/js/main.21f283c1.chunk.js"
+  },
+  {
+    "revision": "552d24ed4e1c803a87f6",
+    "url": "/static/js/runtime-main.085de968.js"
+  },
+  {
+    "revision": "8b522d2945ece3a1543c4ccc7d918e4c",
+    "url": "/static/media/africa.8b522d29.jpg"
+  },
+  {
+    "revision": "a5ecd55f672796faa51da19146fb7bf2",
+    "url": "/static/media/asia.a5ecd55f.jpg"
+  },
+  {
+    "revision": "8fb2fd6c4819b6a6afb96ed9bbc48bf7",
+    "url": "/static/media/australia.8fb2fd6c.jpg"
+  },
+  {
+    "revision": "47365a0696d4c9474ef551917d1d2f6f",
+    "url": "/static/media/bg.47365a06.jpg"
+  },
+  {
+    "revision": "2106e13a13f0c02df0f4fdc90793139c",
+    "url": "/static/media/cdc.2106e13a.jpeg"
+  },
+  {
+    "revision": "5d8ab87eaccb2670dfdb36384c9e0321",
+    "url": "/static/media/europe.5d8ab87e.jpg"
+  },
+  {
+    "revision": "7b26ec9e8d677d2740b28c4ac4428c37",
+    "url": "/static/media/hhs.7b26ec9e.jpg"
+  },
+  {
+    "revision": "c50dfc3b586dbbddfef23417d575ac46",
+    "url": "/static/media/logo.c50dfc3b.png"
+  },
+  {
+    "revision": "48d23fba06d5775656c978d518a9657e",
+    "url": "/static/media/north-america.48d23fba.jpg"
+  },
+  {
+    "revision": "149a2913691375f751ff82a51df8c2aa",
+    "url": "/static/media/south-america.149a2913.jpg"
+  },
+  {
+    "revision": "ba61b1bf16e7710034b7f1cbc9380f48",
+    "url": "/static/media/who.ba61b1bf.jpg"
+  }
+]);
--- src/components/Resources/Resource.js
+++ src/components/Resources/Resource.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardActionArea from "@material-ui/core/CardActionArea";
@@ -8,7 +8,6 @@ import CardMedia from "@material-ui/core/CardMedia";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import styles from './Resources.module.css';
-import CloseIcon from "@material-ui/icons/Close";
 
 const useStyles = makeStyles({
   media: {
@@ -18,35 +17,10 @@ const useStyles = makeStyles({
 
 const Resource = ({title, infoSource, image, description, url}) => {
   const classes = useStyles();
-  const [clicked, setClicked] = useState(false);
 
   return (
     <Card className={styles.resource}>
-      <div
-        className={styles.resource__overlap}
-        style={{ display: clicked ? "block" : "none" }}
-      >
-        <div className={styles.resource__overlap__content}>
-          <div className={styles.resource__overlap__icon}>
-            <CloseIcon
-              className={styles.close__icon}
-              onClick={() => setClicked(false)}
-            />
-          </div>
-          <CardActionArea>
-            <CardMedia className={classes.media} image={image} title={title} />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="h2">
-                {infoSource}
-              </Typography>
-              <Typography variant="body2" color="textSecondary" component="p">
-                {description}
-              </Typography>
-            </CardContent>
-          </CardActionArea>
-        </div>
-      </div>
-      <CardActionArea onClick={() => setClicked(true)}>
+      <CardActionArea>
         <CardMedia className={classes.media} image={image} title={title} />
         <CardContent>
           <Typography gutterBottom variant="h5" component="h2">
